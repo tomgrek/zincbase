2baaf7a8be066429b5f38df1d0400e48f33994b7
Tom Grek <tom.grek@gmail.com>
2019-04-28 16:46:29 -0700

    fix rtd

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
-autodoc_mock_imports = ['matplotlib', 'torch']
+autodoc_mock_imports = ['matplotlib', 'torch', 'numpy', 'networkx', 'scipy', 'sklearn']
 # -- Project information -----------------------------------------------------
 
 project = 'XinKBase'
