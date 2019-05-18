d1e04e564aba0dfa49f0beabc1cb0ab8821570bd
Tom Grek <tom.grek@gmail.com>
2019-05-17 18:47:20 -0700

    Hopefully fix doc build on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
-autodoc_mock_imports = ['matplotlib', 'torch', 'numpy', 'networkx', 'scipy', 'sklearn']
+autodoc_mock_imports = ['matplotlib', 'torch', 'numpy', 'networkx', 'scipy', 'sklearn', 'tqdm']
 # -- Project information -----------------------------------------------------
 
 project = 'ZincBase'
